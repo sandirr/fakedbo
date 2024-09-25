f139bc6f06039be430bab5bba9f4ca88719b359f
Andi Irsandi Ramadani <andi@dbo.id>
2024-09-25 14:07:59 +0700

    update json server file

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -1,9 +1,22 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Path ke db.json
+const server = jsonServer(); // Correct function to create server
+const router = jsonServer.router('db.json'); // Path to db.json
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports.handler = server;
+const handler = async (event, context) => {
+  // Required for Netlify functions to handle events and context
+  return new Promise((resolve, reject) => {
+    server.handle(event, context, (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+};
+
+module.exports = { handler };
